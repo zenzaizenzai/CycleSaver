4c5c65422f7ed73614845d6e4fc6c9fb66f7665f
zenzai <zenzaizenzai@outlook.com>
2025-12-01 21:00:41 +0900

    デプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 🚀 ここに base プロパティを追加します
+      base: '/CycleSaver/', // GitHub Pagesのリポジトリ名を設定
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
